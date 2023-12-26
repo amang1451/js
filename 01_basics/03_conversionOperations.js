d31320c46ad13a9adbaccc4e08be36e0af55f2f5
Aman Gupta <60704540+amang1451@users.noreply.github.com>
2023-12-26 06:43:54 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
 let score =  "23ac"
 
-console.log(typeof score);
-console.log(typeof(score))
+// console.log(typeof score);
+// console.log(typeof(score))
 
 let valueInNumber =  Number(score)
-console.log(valueInNumber)
-console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+// console.log(typeof(valueInNumber))
 
 // "33" => 33 (Number)
 // "33abc" =>  NaN (Number)
@@ -15,8 +15,8 @@ console.log(typeof(valueInNumber))
 
 let isLoggedIn = 3
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
-console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
 
 // other than 0 => true (boolean)
 // "" => false
@@ -27,5 +27,52 @@ console.log(typeof booleanIsLoggedIn)
 
 let someNumber = 32
 let stringSomeNumber = Number(someNumber)
-console.log(stringSomeNumber)
-console.log(typeof stringSomeNumber)
+// console.log(stringSomeNumber)
+// console.log(typeof stringSomeNumber)
+
+// -------------operations-----------------
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**3) // 2 is to power 3
+
+// let str1 = "Hello"
+// let str2 = " Aman"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(2 + 2 + "1");
+
+// console.log(true);
+// console.log(+true); //equal to 1
+// console.log(+"")
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+1;
+
+let gameCounter = 100
+// console.log(++gameCounter);
+// console.log(gameCounter++);
+
+let x = 3;
+const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3" 
+//  the increment operator increments and returns the value before incrementing. in postfix
+
+let a = 3;
+const b = ++a;
+// he increment operator increments and returns the value after incrementing. in prefix
+
+// console.log(`a:${a}, b:${b}`);
