7cfc4652caa7de175e066909dd0063d79c80b6c5
Aman Gupta <60704540+amang1451@users.noreply.github.com>
2023-12-26 07:00:20 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log(2<1);
+
+// console.log("1" > 1); // automatically convert string to number
+// console.log("02" > 1); // automatically convert string to number
+
+// null in comparison check(null becomes 0) and equality check work differently
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined <= 0);
+console.log(undefined > 0);
+
+//  strict check === -> check datatypes in comparison also
+
+console.log("2" === 2)
